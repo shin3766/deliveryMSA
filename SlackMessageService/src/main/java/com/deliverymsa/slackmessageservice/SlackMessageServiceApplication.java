31a842d174eacafd2c4a9ee40fb418a875b9df93
Yoosup Shin <brian96shin@gmail.com>
2024-09-10 15:03:51 +0900

    git init

---
+++ SlackMessageService/src/main/java/com/deliverymsa/slackmessageservice/SlackMessageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deliverymsa.slackmessageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackMessageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackMessageServiceApplication.class, args);
+	}
+
+}
---
+++ SlackMessageService/src/test/java/com/deliverymsa/slackmessageservice/SlackMessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverymsa.slackmessageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackMessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
