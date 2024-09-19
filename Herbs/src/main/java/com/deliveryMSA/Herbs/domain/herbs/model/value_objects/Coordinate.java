7d0daf2a2f225036ded58e1ae89a116433f1846d
Yoosup Shin <brian96shin@gmail.com>
2024-09-19 15:58:05 +0900

    feat: 좌표 value-object화

---
+++ Herbs/src/main/java/com/deliveryMSA/Herbs/domain/herbs/model/value_objects/Coordinate.java
@@ -0,0 +1,22 @@
+package com.deliveryMSA.Herbs.domain.herbs.model.value_objects;
+
+import jakarta.persistence.Column;
+
+public class Coordinate {
+    @Column(name = "latitude")
+    private double latitude;
+    @Column(name = "longitude")
+    private double longitude;
+
+    protected Coordinate() {}; // JPA only
+
+    public Coordinate(double latitude, double longitude) {
+
+        if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180) {
+            throw new IllegalArgumentException("Invalid coordinate");
+        }  // Validation logic can be added here if needed.
+
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
